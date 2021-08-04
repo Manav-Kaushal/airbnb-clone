006a9e40aa40bfb80de1c99ef5743355053e5040
Manav Kaushal <manavkaushal756@gmail.com>
2021-08-05 03:22:09 +0530

    banner complete

---
+++ components/Banner.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Image from "next/image";
+
+const Banner = () => {
+  return (
+    <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] xxl:h-[700px]">
+      <Image
+        src="https://links.papareact.com/0fm"
+        layout="fill"
+        objectFit="cover"
+        objectPosition="bottom"
+      />
+      <div className="absolute w-full text-center top-1/2">
+        <p className="text-sm sm:text-lg">Not sure where to go? Perfect!</p>
+        <button className="px-10 py-4 my-3 text-red-600 transition duration-150 bg-white rounded-full shadow-md hover:shadow-lg active:scale-90">
+          I'm flexible
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 
 export default function Home() {
@@ -8,7 +9,7 @@ export default function Home() {
         <title>Welcome To AirBnb</title>
       </Head>
       <Header />
-      {/* Banner */}
+      <Banner />
     </div>
   );
 }
