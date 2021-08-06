76879d0e2ab582a5af00a8276accbd536d3bcbee
Manav Kaushal <manavkaushal756@gmail.com>
2021-08-07 03:42:19 +0530

    components updated

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -7,13 +7,14 @@ const Banner = () => {
       <Image
         src="https://links.papareact.com/0fm"
         layout="fill"
+        alt=""
         objectFit="cover"
         objectPosition="bottom"
       />
       <div className="absolute w-full text-center top-1/2">
         <p className="text-sm sm:text-lg">Not sure where to go? Perfect!</p>
         <button className="px-10 py-4 my-3 text-red-600 transition duration-150 bg-white rounded-full shadow-md hover:shadow-lg active:scale-90">
-          I'm flexible
+          I&apos;m flexible
         </button>
       </div>
     </div>
